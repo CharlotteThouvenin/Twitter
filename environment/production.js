bc0ee3348ed089a8ece94aa697d514ba5219a623
Charlotte Thouvenin <charlotte_thouvenin@hotmail.fr>
2024-05-30 14:21:04 +0200

    Update production.js

--- environment/production.js
+++ environment/production.js
@@ -3,5 +3,5 @@ const path = require('path')
 module.exports = {
     dburl: 'mongodb+srv://charlotte:charlotte@cluster0.z36rcvr.mongodb.net/Twitter?retryWrites=true&w=majority&appName=Cluster0',
     cert: '/etc/letsencrypt/live/www.charlottethouvenin-dev.fr/fullchain.pem',
-    key: ' /etc/letsencrypt/live/www.charlottethouvenin-dev.fr/privkey.pem'
+    key: '/etc/letsencrypt/live/www.charlottethouvenin-dev.fr/privkey.pem'
 }
