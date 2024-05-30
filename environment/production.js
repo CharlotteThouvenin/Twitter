e2da0d5457ad2fb1fbe91661de4f11ba3a19db6e
Charlotte Thouvenin <charlotte_thouvenin@hotmail.fr>
2024-05-30 14:04:12 +0200

    certificat

--- environment/production.js
+++ environment/production.js
@@ -2,6 +2,6 @@ const path = require('path')
 
 module.exports = {
     dburl: 'mongodb+srv://charlotte:charlotte@cluster0.z36rcvr.mongodb.net/Twitter?retryWrites=true&w=majority&appName=Cluster0',
-    cert: path.join(__dirname, ''),
-    key: path.join(__dirname, '')
+    cert: '/etc/letsencrypt/live/www.charlottethouvenin-dev.fr/fullchain.pem',
+    key: ' /etc/letsencrypt/live/www.charlottethouvenin-dev.fr/privkey.pem'
 }
